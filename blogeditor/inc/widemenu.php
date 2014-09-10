2ecd4887b81bb0f7ebfa15accb45f17096080cbf
jaredblyth <jaredblyth@hotmail.com>
2014-09-10 18:57:09 +1000

    Updated widemenu

--- blogeditor/inc/widemenu.php
+++ blogeditor/inc/widemenu.php
@@ -2,7 +2,7 @@
 <ul>
 <li><a href="logout.php">Logout</a></li>
 <li><a href="newsletter-create.php">Create Newsletter</a></li>
-<li><a href="https://ex.myhosting.com/owa/auth/logon.aspx" target="_blank">Email</a></li>
+<li><a href="#" target="_blank">Email</a></li>
 <?php include ('admin/phpmyadmin.php');?>
 <li><a href="view-comments.php">Comments</a></li>
 <li><a href="view-subscribers.php">Subscribers</a></li>
